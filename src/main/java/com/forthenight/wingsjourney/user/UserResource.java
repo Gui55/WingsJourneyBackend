27620934de7dabb7830f1d41f3c89d41dade8fea
guilhermemedeirosmergulhao <gui.mergulhao55@outlook.com>
2024-06-07 20:48:04 -0300

    Role funcionando com JWT (links de testes)

--- src/main/java/com/forthenight/wingsjourney/user/UserResource.java
+++ src/main/java/com/forthenight/wingsjourney/user/UserResource.java
@@ -1,10 +1,19 @@
 package com.forthenight.wingsjourney.user;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,10 +62,33 @@ public class UserResource {
             0,
             userData.getUsername(),
             userData.getEmail(),
-            passwordEncoder.encode(userData.getPassword())
+            passwordEncoder.encode(userData.getPassword()),
+            userData.getRole()
         );
         repository.save(user);
         return ResponseEntity.ok(userData);
+    }
+
+    @GetMapping("users/role")
+    public Map<String,Object> getPrincipalInfo(JwtAuthenticationToken token) {
+        
+        Collection<String> authorities = token.getAuthorities()
+          .stream()
+          .map(GrantedAuthority::getAuthority)
+          .collect(Collectors.toList());
+        
+        Map<String,Object> info = new HashMap<>();
+        info.put("name", token.getName());
+        info.put("authorities", authorities);
+        info.put("tokenAttributes", token.getTokenAttributes());
+
+        return info;
+    }
+
+    @GetMapping("games/test")
+    @PreAuthorize("hasAuthority('ROLE_KING')")
+    public String testLink(){
+        return "A test link";
     }
 
 }
